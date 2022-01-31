8d0714813caefafeeea1baadcc9b19516cc2cbe4
root <sunnexbanty@gmail.com>
2022-01-31 15:08:04 +0000

    kk

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 """
-Write a Python script that, using this REST API,
-for a given employee ID, returns information about
-his/her TODO list progress
+REST API
 """
 import requests
 import sys
